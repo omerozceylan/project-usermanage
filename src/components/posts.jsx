86bd6f807b476b4f9cd04fe2def5cc52b4b10b4c
Omer Ozceylan <omerozceylan35@gmail.com>
2023-05-10 01:16:17 +0300

    feat: posts component work perfectly

--- src/components/posts.jsx
+++ src/components/posts.jsx
@@ -1,21 +1,56 @@
 import axios from "axios"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 
 export default function Posts() {
+    const [user, setUser] = useState('')
+    const [posts, setPosts] = useState([])
+
     const  {id} = useParams()
     const separatedId = id.slice(id.indexOf(':')+1)
     useEffect(()=> {
+        async function getUserInfo() {
+            const {data: user} = await axios('https://jsonplaceholder.typicode.com/users/'+separatedId)
+            setUser(user)
+        }
         async function getPostsById() {
             const {data: posts} = await axios('https://jsonplaceholder.typicode.com/posts?userId='+separatedId)
             console.log(posts)
+            setPosts(posts)
         }
+        getUserInfo()
         getPostsById()
     },[])
     return (
-        
         <>
-            <h1>burası posts {id}</h1>
+            <div className="posts-container">
+                <div className="title-zone">
+
+                <h1>{user.name}</h1>
+                </div>
+                <table>
+                <thead className="header">
+                    <tr>
+                        <th>postId</th>
+                        <th>userId</th>
+                        <th>Title</th>
+                        <th>content</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {posts.map((posts,key)=>(
+                        <tr key={key}>
+                            <td>{posts.id}</td>
+                            <td>{posts.userId}</td>
+                            <td>{posts.title}</td>
+                            <td>{posts.body}</td>
+                         </tr>
+                    ))}
+                    
+                  
+                </tbody>
+                </table>
+            </div>
         </>
     )
 
